20aa5608dcf2fa34389990ff42f7bded3d66333a
Bryan Alvarez <alvarez.bryan79@yahoo.com>
2021-07-22 17:35:28 -0700

    completed movie app

--- movie-app/script.js
+++ movie-app/script.js
@@ -2,6 +2,7 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH='https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=bedfd2ea5bf7a6a8d1894aae7e9f7e56&query="';
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
@@ -9,9 +10,42 @@ async function getMovies(url) {
     const res = await fetch(url);
     const data = await res.json()
 
-    console.log(data.results);
+    showMovies(data.results);
 }
 
+function showMovies(movies) {
+    main.textContent = '';
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie;
+
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>${overview}</h3>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, voluptatum maiores illo nisi qui nobis odit architecto quibusdam minima corrupti impedit eum eos ad officiis dignissimos culpa rerum id perferendis!
+            </div>
+        `
+
+        main.appendChild(movieEl);
+    })
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green'
+    } else if (vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -29,4 +63,4 @@ form.addEventListener('submit', (e) => {
 
 // On Load
 // Get initial Movies
-// getMovies(API_URL);
+getMovies(API_URL);
