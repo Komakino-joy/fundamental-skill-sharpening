4208ec86254e6af620c3b47f0476093c057a0c54
Bryan Alvarez <Bryan.Alvarez@genco.com>
2021-07-26 11:57:41 -0700

    completed github profiles project

--- github-api/script.js
+++ github-api/script.js
@@ -1,5 +1,6 @@
 const API_URL = 'https://api.github.com/users/';
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
@@ -7,10 +8,78 @@ async function getUser(username) {
 
     try {
         const { data } = await axios.get(API_URL + username);
-        console.log(data);
+        createUserCard(data);
+        getRepos(username);
     } catch (error) {
-        console.log(error);
+        if (error.response.status === 404){
+            createErrorCard('No profile with this username was found.');
+        }
     }
+}
+
+async function getRepos(username) {
+    try {
+        const { data } = await axios.get(API_URL + username + '/repos?sort=created');
+        addReposToCard(data);
+        
+    } catch (error) {
+            createErrorCard('Problem fetching repos.');
+    }
+}
+
+function createUserCard(user) {
+
+    const { avatar_url, name, bio, followers, following, public_repos } = user;
+
+    const cardHTML = `
+    <div class="card">
+        <div>
+            <img src="${avatar_url}" alt="${name}" class="avatar">
+        </div>
+        <div class="user-info">
+            <div>
+                <h2>${name}</h2>
+                <p>${bio}</p>
+            </div>
+
+            <ul>
+                <li>${followers}<strong> Followers &nbsp; </strong></li>
+                <li>${following}<strong> Following &nbsp;</strong></li>
+                <li>${public_repos}<strong> Repos  &nbsp;</strong></li>
+            </ul>
+
+            <div id="repos"> </div>
+        </div>
+    </div>
+    `;
+
+    main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+    const cardHTML = `
+        <div class="card">
+            <h1>${msg}</h1>
+        </div>
+    `;
+
+    main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos');
+
+    repos
+    .slice(0,10)
+    .forEach(repo => {
+        const repoEl = document.createElement('a');
+        repoEl.classList.add('repo');
+        repoEl.href = repo.html_url;
+        repoEl.target = '_blank';
+        repoEl.textContent = repo.name;
+
+        reposEl.appendChild(repoEl);
+    });
 }
 
 form.addEventListener('submit', (e) => {
